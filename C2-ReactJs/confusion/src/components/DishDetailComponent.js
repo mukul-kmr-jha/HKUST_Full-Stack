0fb11266a90d3057452fa62eed5d1d82537a15df
mukul-kmr-jha <jimickeyji@gmail.com>
2019-05-30 21:59:47 +0530

    Header and Footer

--- C2-ReactJs/confusion/src/components/DishDetailComponent.js
+++ C2-ReactJs/confusion/src/components/DishDetailComponent.js
@@ -6,7 +6,7 @@ function RenderDish({ dish }) {
     return (
         <div className='col-12 col-md-5 m-1'>
             <Card>
-                <CardImg bottom src={dish.image} alt={dish.name} />
+                <CardImg bottom width="100%" src={dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
