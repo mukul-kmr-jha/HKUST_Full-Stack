3f81b552cdcd8fe4948b272d72cdff34f8cf065e
mukul-kmr-jha <jimickeyji@gmail.com>
2019-07-12 12:25:27 +0530

    Redux Thunk and Logger excerise

--- C2-ReactJs/confusion/src/components/DishDetailComponent.js
+++ C2-ReactJs/confusion/src/components/DishDetailComponent.js
@@ -2,7 +2,7 @@ import React ,{ Component }from 'react';
 import { Card, CardBody, CardImg, CardTitle, CardText, Breadcrumb, BreadcrumbItem, Button, ButtonGroup, Modal, Row, Col, ModalHeader, Label, ModalBody, ModalFooter, Input, FormGroup }  from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
     return (
@@ -134,7 +134,25 @@ function RenderComments({ comments, addComment, dishId }) {
 
 const DishDetail =  (props) => {
 
-        if (props.dish != null){
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         console.log(props);
         return (
             <div className="container">
