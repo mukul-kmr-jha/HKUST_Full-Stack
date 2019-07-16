def24cc9da6148c9ad6de3bef998c8ffcc235e4b
mukul-kmr-jha <jimickeyji@gmail.com>
2019-07-17 00:02:28 +0530

    Error handling Exercise in fetching data from server

--- C2-ReactJs/confusion/src/redux/ActionCreators.js
+++ C2-ReactJs/confusion/src/redux/ActionCreators.js
@@ -1,23 +1,37 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
-import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-    }
+  type: ActionTypes.ADD_COMMENT,
+  payload:{
+    dishId:dishId,
+    rating:rating,
+    author:author,  
+    comment:comment
+  }  
 });
-// this is a thunk (as it returns a function instead of a action object)
-export const fetchDishes = () =>(dispatch) =>{
 
-    return fetch(baseUrl + 'dishes')
+// this is a thunk (as it returns a function instead of a action object)
+
+export const fetchComments = () => (dispatch) => {    
+    return fetch(baseUrl + 'comments')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));
-}
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
+};
 // these are action creators 
 export const dishesLoading = () => ({
     type: ActionTypes.DISHES_LOADING
@@ -32,12 +46,6 @@ export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
 });
-
-export const fetchComments = () => (dispatch) => {    
-    return fetch(baseUrl + 'comments')
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
-};
 
 export const commentsFailed = (errmess) => ({
     type: ActionTypes.COMMENTS_FAILED,
@@ -54,8 +62,44 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
     .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
+}
+export const fetchDishes = () => (dispatch) => {
+
+  dispatch(dishesLoading(true));
+
+  return fetch(baseUrl + 'dishes')
+  .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        var error = new Error('Error ' + response.status + ': ' + response.statusText);
+        error.response = response;
+        throw error;
+      }
+    },
+    error => {
+          var errmess = new Error(error.message);
+          throw errmess;
+    })
+  .then(response => response.json())
+  .then(dishes => dispatch(addDishes(dishes)))
+  .catch(error => dispatch(dishesFailed(error.message)));
 }
 
 export const promosLoading = () => ({
